b1820e4b0c1d6b4984c88b404322ffed55fccafc
Chris-CCW <cyberfilth@protonmail.com>
2019-03-01 13:45:16 +0000

    Updated code to reflect coding standards

--- OutputOfAnalysis/sendkeywordcounttofrontend.js
+++ OutputOfAnalysis/sendkeywordcounttofrontend.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 2008;
+const EXPRESS = require('express');
+const APP = EXPRESS();
+const PORT = 2008;
 let fs = require('fs'),
 json;
 
-
-
-
-
-module.exports.returnOutput =  function(){
-let rawOutput = fs.readFileSync('exampleoutput.json')
-let output = JSON.parse(rawOutput);
-    //send to front end//
-app.get('/',(req,res)=>res.send(output));
-app.listen(port,function(){
-    console.log(`listening on port ${port}`);
-})
-
+module.exports.returnOutput = function() {
+  let rawOutput = fs.readFileSync('exampleoutput.json')
+  let output = JSON.parse(rawOutput);
+  //send to front end//
+  APP.get('/',(req,res)=>res.send(output));
+  APP.listen(PORT,function() {
+    console.log(`listening on port ${PORT}`);
+  })
 }
-
-
